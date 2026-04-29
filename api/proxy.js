1416ee4cd2f1f3c265cb2ed94beee1d7f5df7c79
DanielBVisions <daniel.bate@visionsdesign.co.uk>
2026-04-29 16:58:56 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,11 +1,11 @@
 export default async function handler(req, res) {
   const { slug } = req.query;
   const TOKEN = 'd85161e0dd2a33a196a1bea7b2d9d493a182c8ead6cb0f1816d0721459203da3';
-  const COLLECTION_ID = '69f0c5f8a03008cb9747ec85';
+  const COLLECTION_ID = '69f0c5f8a03008cb9747ec7f';
   const SITE_ID = '69c11e3a62cbd5490546ec0f';
 
   const response = await fetch(
-    `https://api.webflow.com/v2/sites/${SITE_ID}/collections`,
+    `https://api.webflow.com/v2/sites/${SITE_ID}/collections/${COLLECTION_ID}/items?slug=${slug}`,
     {
       headers: {
         Authorization: `Bearer ${TOKEN}`,
