954626ee2bf735fe97294f42b64009d93baeac74
DanielBVisions <daniel.bate@visionsdesign.co.uk>
2026-04-30 10:49:14 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -4,10 +4,6 @@ export default async function handler(req, res) {
   const COLLECTION_ID = '69f0c5f8a03008cb9747ec7f';
   const SITE_ID = '69c11e3a62cbd5490546ec0f';
 
-  if (!slug) {
-    return res.status(400).json({ error: 'Missing slug' });
-  }
-
   const response = await fetch(
     `https://api.webflow.com/v2/sites/${SITE_ID}/collections/${COLLECTION_ID}/items?slug=${slug}`,
     {
@@ -19,8 +15,7 @@ export default async function handler(req, res) {
   );
 
   const data = await response.json();
-  const html = data?.items?.[0]?.fieldData?.['artifact-html'] ?? null;
 
   res.setHeader('Access-Control-Allow-Origin', '*');
-  return res.status(200).json({ html });
+  return res.status(200).json(data);
 }
